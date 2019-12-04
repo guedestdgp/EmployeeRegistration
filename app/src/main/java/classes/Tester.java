e053af755652ff1256977773b22b9de34081b88e
Tatiane <guedestdgp@outlook.com>
2019-12-03 20:26:16 -0400

    Created Tester class.

---
+++ app/src/main/java/classes/Tester.java
@@ -0,0 +1,28 @@
+package classes;
+
+public class Tester extends Employee {
+    private int nbBugs;
+
+    private final int gain_factor_error;
+
+    {
+        gain_factor_error = 10;
+    }
+
+    protected Tester(int employeeId) {
+        super(employeeId);
+    }
+
+    @Override
+    void toDisplay() {
+
+    }
+
+    public int getNbBugs() {
+        return nbBugs;
+    }
+
+    public void setNbBugs(int nbBugs) {
+        this.nbBugs = nbBugs;
+    }
+}
