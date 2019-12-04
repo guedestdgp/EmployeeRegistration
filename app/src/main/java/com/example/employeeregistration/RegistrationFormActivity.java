19140836fb2c3908be3446fff4b4377fb1776f8e
Tatiane <guedestdgp@outlook.com>
2019-12-04 13:15:27 -0400

    Created method vehicleChecked for radiogroup. The proposes is changing visible to gone and vice-versa.

--- app/src/main/java/com/example/employeeregistration/RegistrationFormActivity.java
+++ app/src/main/java/com/example/employeeregistration/RegistrationFormActivity.java
@@ -3,6 +3,10 @@ package com.example.employeeregistration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 /*--- Tatiane's Project - C0755772 ---*/
 public class RegistrationFormActivity extends AppCompatActivity {
@@ -11,5 +15,20 @@ public class RegistrationFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_form);
+    }
+
+    public void vehicleChecked(View view){
+        RadioGroup radioGroup = findViewById(R.id.rg_vehicle);
+        TableRow rowCar = findViewById(R.id.rowCar);
+        TableRow rowMotorbike = findViewById(R.id.rowMotorbike);
+        int id = radioGroup.getCheckedRadioButtonId();
+        switch (id) {
+            case R.id.car:
+                rowCar.setVisibility(View.VISIBLE);
+                rowMotorbike.setVisibility(View.GONE);
+            case R.id.motorbike:
+                rowCar.setVisibility(View.GONE);
+                rowMotorbike.setVisibility(View.VISIBLE);
+        }
     }
 }
