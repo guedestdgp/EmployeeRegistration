014fdd4699549a490454112d5bfbd8ac03f839d8
Tatiane <guedestdgp@outlook.com>
2019-12-03 20:21:53 -0400

    Created package classes. Created Vehicle class.

---
+++ app/src/main/java/classes/Vehicle.java
@@ -0,0 +1,55 @@
+package classes;
+
+abstract class Vehicle {
+    private int vehicleId;
+    private String make;
+    private String plate;
+    private String color;
+    private String category;
+
+    abstract void toDisplay();
+
+    protected Vehicle(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
