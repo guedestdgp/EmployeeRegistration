b866bd9dd4668cc897818929cefc80d63bb7e623
Tatiane <guedestdgp@outlook.com>
2019-12-03 20:24:44 -0400

    Created Manager class.

---
+++ app/src/main/java/classes/Manager.java
@@ -0,0 +1,34 @@
+package classes;
+
+public class Manager extends Employee {
+    private int nbClients;
+
+    private final int gain_factor_client;
+
+    {
+        gain_factor_client = 500;
+    }
+
+    private final int gain_factor_travel;
+
+    {
+        gain_factor_travel = 100;
+    }
+
+    protected Manager(int employeeId) {
+        super(employeeId);
+    }
+
+    @Override
+    void toDisplay() {
+
+    }
+
+    public int getNbClients() {
+        return nbClients;
+    }
+
+    public void setNbClients(int nbClients) {
+        this.nbClients = nbClients;
+    }
+}
